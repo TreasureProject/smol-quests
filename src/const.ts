3450b7c0c43430b9343f1aca10d324e11f7a4d96
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-06-02 08:53:26 -0700

    new contract addresses

--- src/const.ts
+++ src/const.ts
@@ -17,8 +17,8 @@ export const CONTRACT_ADDRESSES: Record<string, ContractMap> = {
   [chain.arbitrumRinkeby.name]: {
     [AppContract.SmolBrains]: "0x2542421ACA04A98f5Cf04DA97a36DAD8F1FaC3f4",
     [AppContract.SmolTreasures]: "0x8e79c8607a28fe1EC3527991C89F1d9E36D1bAd9",
-    [AppContract.WrappedSmols]: "0x112001Ab4FE40464cAc3Cf789E4cd304176051B0",
-    [AppContract.EpisodeOne]: "0x0536490B709248543c897983eaABD1f586143c51",
+    [AppContract.WrappedSmols]: "0x939b27773AB84454Bd921DFb20B2c43e23271F56",
+    [AppContract.EpisodeOne]: "0xbb6513F84702827A68b71dBD762E41A91C3b9af5",
   },
   [chain.arbitrum.name]: {
     [AppContract.SmolBrains]: "0x6325439389E0797Ab35752B4F43a14C004f22A9c",
